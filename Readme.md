749943a3f0e707b0340f734e8a1ed62f6318fc01
puneet <imhtcy312@gmail.com>
2024-12-22 19:53:32 +0530

    updated readme

---
+++ Readme.md
@@ -0,0 +1,2 @@
+# Collaborative Blog Production
+[Planning](https://docs.google.com/document/d/14tBFxRKkkT5YaSt7v-_mfqNhStxEcoZdh2FGcqw3zUQ/edit?usp=sharing)
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,8 @@
 import "./index.css";
 function App() {
-  return <p className="text-red-700 text-3xl font-bold underline">Hello</p>;
+  return <p className="text-red-700 text-3xl font-bold underline">
+        
+  </p>;
 }
 
 export default App;
